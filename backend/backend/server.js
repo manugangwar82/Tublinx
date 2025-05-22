88f019f297dc2b4b25da7a57491490b4ee9b11b0
manugangwar82 <mannukurmi97613@gmail.com>
2025-05-22 21:53:03 +0530

    Update server.js

--- backend/backend/server.js
+++ backend/backend/server.js
@@ -120,7 +120,10 @@ require("dotenv").config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+  origin: "https://tublinx.onrender.com"  // ✅ Sirf apne frontend ko allow karo
+}));
 app.use(express.json());
 
 const urlDatabase = {}; // URL Shortening Storage
